98e820f960a73d3e8b513077f2c0f4bb11fdc947
Mykola Nikishov <mn@mn.com.ua>
2010-07-05 23:21:55 +0300

    Apache License boilerplate
    Signed-off-by: Mykola Nikishov <mn@mn.com.ua>

--- db-migration-maven-plugin/src/main/java/com/carbonfive/db/migration/maven/AbstractMigrationMojo.java
+++ db-migration-maven-plugin/src/main/java/com/carbonfive/db/migration/maven/AbstractMigrationMojo.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2008, 2010 Christian Nelson
+ * Copyright (c) 2010 Mykola Nikishov <mn@mn.com.ua>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.carbonfive.db.migration.maven;
 
 import com.carbonfive.db.jdbc.DatabaseType;
